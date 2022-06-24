931109749f719607c8ead0ed4b7a54d7350923d9
xiao <okxiaochen@outlook.com>
2022-06-25 03:02:10 +0800

    config: update mongodb connections

--- tracker/app.py
+++ tracker/app.py
@@ -5,9 +5,15 @@ from tracker.const import CONFIG
 
 app = Flask(__name__)
 app.secret_key = "eawfopawjfoawe"
+
 app.config['MONGODB_SETTINGS'] = {
-    'host': CONFIG["mongoDBUri"],
-    'connect': False,
+    "host": CONFIG["mongo_host"],
+    "port": CONFIG["mongo_port"],
+    "db": CONFIG["mongo_db"],
+    "username": CONFIG["mongo_user"],
+    "password": CONFIG["mongo_password"],
+    "connect": False,
 }
+
 db = MongoEngine(app)
 app.debug = CONFIG["debug"]
